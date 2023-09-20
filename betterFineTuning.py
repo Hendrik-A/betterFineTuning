2b1e68d9cec45c5d1240aeedd6bf98b5da34e227
Hendrik-A <145358341+Hendrik-A@users.noreply.github.com>
2023-09-20 14:58:41 +0200

    Update betterFineTuning.py
    handle train, test, valid documents

--- betterFineTuning.py
+++ betterFineTuning.py
@@ -1,4 +1,5 @@
 import os
+import argparse
 import math
 import pyspark
 from pyspark.sql import functions as F
@@ -33,7 +34,13 @@ def getTwo():
     return 2
 def getThree():
     return 3
-	
+
+def read_args():
+    parser = argparse.ArgumentParser()
+    parser.add_argument("--data_root", type=str, help="")
+
+    args, unknown = parser.parse_known_args()
+    return args, unknown
 
 def main():
 
@@ -42,10 +49,17 @@ def main():
 	sc = pyspark.SparkContext(conf=conf)
 	spark = pyspark.sql.SparkSession(sc)
 
-	path = "/home/hamann/dataset/better/"
+	args, unknown = read_args()
 
-	data_path = os.path.join(path, 'train.txt')
-	task_output_dir = os.path.join(path, "processed")
+	train_data = os.path.join(args.data_root, 'train.txt')
+    	val_data = os.path.join(args.data_root, 'val.txt')
+    	test_data = os.path.join(args.data_root, 'test.txt')
+
+	data_prefixes = ['train', 'val', 'test']
+    	data_paths = [train_data, val_data, test_data]
+	
+	data_path = os.path.join(args.data_root, 'train.txt')
+	task_output_dir = os.path.join(args.data_root, "processed/betterFineTuning")
 	if not os.path.exists(task_output_dir):
 		os.makedirs(task_output_dir)
 	
@@ -55,25 +69,25 @@ def main():
 	getThree_udf = F.udf(getThree, spark_types.IntegerType())
 	topX_udf = F.udf(topX, spark_types.ArrayType(spark_types.StringType()))
 
+	for data_path, prefix in zip(data_paths, data_prefixes):
 
-	df = spark.read.json(data_path) \
-				.repartition(500, "article_id")
+		df = spark.read.json(data_path) \
+					.repartition(500, "article_id")
 	
-	df = df.drop("labels", "section_names", "sections")
+		df = df.drop("labels", "section_names", "sections")
 	
-	df = df.withColumn("abstract", F.concat_ws(" ", F.col("abstract_text"))).withColumn("abstract", F.regexp_replace("abstract", "<\/?S>", "")).drop("abstract_text")
+		df = df.withColumn("abstract", F.concat_ws(" ", F.col("abstract_text"))).withColumn("abstract", F.regexp_replace("abstract", "<\/?S>", "")).drop("abstract_text")
 
-	df = df.withColumn("sorted_text", sorter_udf(F.col('article_text'), F.col('abstract'))) \
-		.withColumn("article_text", F.concat_ws(" ", F.col("article_text"))) \
-		.withColumn("top25", getOne_udf()).withColumn("top50", getTwo_udf()).withColumn("top75", getThree_udf()) \
-		.withColumn("top25", topX_udf(F.col('sorted_text'), F.col('top25'))).withColumn("top50", topX_udf(F.col('sorted_text'), F.col('top50'))).withColumn("top75", topX_udf(F.col('sorted_text'), F.col('top75'))) \
-		.withColumn("sorted_text", F.concat_ws(" ", F.col("sorted_text"))) \
-		.withColumn("top25", F.concat_ws(" ", F.col("top25"))).withColumn("top50", F.concat_ws(" ", F.col("top50"))).withColumn("top75", F.concat_ws(" ", F.col("top75")))
+		df = df.withColumn("sorted_text", sorter_udf(F.col('article_text'), F.col('abstract'))) \
+			.withColumn("article_text", F.concat_ws(" ", F.col("article_text"))) \
+			.withColumn("top25", getOne_udf()).withColumn("top50", getTwo_udf()).withColumn("top75", getThree_udf()) \
+			.withColumn("top25", topX_udf(F.col('sorted_text'), F.col('top25'))).withColumn("top50", topX_udf(F.col('sorted_text'), F.col('top50'))).withColumn("top75", topX_udf(F.col('sorted_text'), F.col('top75'))) \
+			.withColumn("sorted_text", F.concat_ws(" ", F.col("sorted_text"))) \
+			.withColumn("top25", F.concat_ws(" ", F.col("top25"))).withColumn("top50", F.concat_ws(" ", F.col("top50"))).withColumn("top75", F.concat_ws(" ", F.col("top75")))
 
-	df.write.json(
-		path=task_output_dir,
-		mode="overwrite"
-	)
+		df.write.json(
+            		path=os.path.join(task_output_dir, prefix),
+            		mode="overwrite")
 
 if __name__ == "__main__":
     main()
