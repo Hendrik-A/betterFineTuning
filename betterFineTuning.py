3cec8c8545bf502f73832835fb3717137ff1d649
Hendrik-A <145358341+Hendrik-A@users.noreply.github.com>
2023-09-20 15:07:57 +0200

    Update betterFineTuning.py
    fix intendation again

--- betterFineTuning.py
+++ betterFineTuning.py
@@ -12,21 +12,21 @@ def sorter(text, abstract):
 	metrics = ['rouge1', 'rouge2', 'rougeL']
 	rg_scorer = rouge_scorer.RougeScorer(metrics, use_stemmer=True)
 	for i, sentence in enumerate(text):
-			results = rg_scorer.score(abstract, sentence)
-			sen_scores[i] = results['rouge1'][2] + 2*results['rouge2'][2] + results['rougeL'][2]
+		results = rg_scorer.score(abstract, sentence)
+		sen_scores[i] = results['rouge1'][2] + 2*results['rouge2'][2] + results['rougeL'][2]
 	sorted_ids = [i[0] for i in sorted(enumerate(sen_scores), key=lambda k: k[1], reverse=True)]
 	sorted_text = [text[i] for i in sorted_ids]
 	return sorted_text
 	
 def topX(text, percentile):
 	if percentile == 1:
-			return text[:math.ceil(len(text)/4)]
+		return text[:math.ceil(len(text)/4)]
 	elif percentile == 2:
-			return text[:math.ceil(len(text)/2)]
+		return text[:math.ceil(len(text)/2)]
 	elif percentile == 3:
-			return text[:math.ceil(len(text)/4*3)]
+		return text[:math.ceil(len(text)/4*3)]
 	else:
-			raise Exception("invalid percentile")
+		raise Exception("invalid percentile")
 
 def getOne():
 	return 1
@@ -52,11 +52,11 @@ def main():
 	args, unknown = read_args()
 
 	train_data = os.path.join(args.data_root, 'train.txt')
-		val_data = os.path.join(args.data_root, 'val.txt')
-		test_data = os.path.join(args.data_root, 'test.txt')
+	val_data = os.path.join(args.data_root, 'val.txt')
+	test_data = os.path.join(args.data_root, 'test.txt')
 
 	data_prefixes = ['train', 'val', 'test']
-		data_paths = [train_data, val_data, test_data]
+	data_paths = [train_data, val_data, test_data]
 	
 	data_path = os.path.join(args.data_root, 'train.txt')
 	task_output_dir = os.path.join(args.data_root, "processed/betterFineTuning")
@@ -86,8 +86,8 @@ def main():
 			.withColumn("top25", F.concat_ws(" ", F.col("top25"))).withColumn("top50", F.concat_ws(" ", F.col("top50"))).withColumn("top75", F.concat_ws(" ", F.col("top75")))
 
 		df.write.json(
-					path=os.path.join(task_output_dir, prefix),
-					mode="overwrite")
+			path=os.path.join(task_output_dir, prefix),
+			mode="overwrite")
 
 if __name__ == "__main__":
 	main()
