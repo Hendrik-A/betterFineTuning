b1f50b7f661c8c31eb321a28228d1907ce882e81
Hendrik-A <145358341+Hendrik-A@users.noreply.github.com>
2023-09-20 15:05:49 +0200

    Update betterFineTuning.py
    replaced spaces with tabs for consistency

--- betterFineTuning.py
+++ betterFineTuning.py
@@ -8,39 +8,39 @@ from pyspark.sql import types as spark_types
 from rouge_score import rouge_scorer
 
 def sorter(text, abstract):
-    sen_scores = [0] * len(text)
-    metrics = ['rouge1', 'rouge2', 'rougeL']
-    rg_scorer = rouge_scorer.RougeScorer(metrics, use_stemmer=True)
-    for i, sentence in enumerate(text):
-            results = rg_scorer.score(abstract, sentence)
-            sen_scores[i] = results['rouge1'][2] + 2*results['rouge2'][2] + results['rougeL'][2]
-    sorted_ids = [i[0] for i in sorted(enumerate(sen_scores), key=lambda k: k[1], reverse=True)]
-    sorted_text = [text[i] for i in sorted_ids]
-    return sorted_text
+	sen_scores = [0] * len(text)
+	metrics = ['rouge1', 'rouge2', 'rougeL']
+	rg_scorer = rouge_scorer.RougeScorer(metrics, use_stemmer=True)
+	for i, sentence in enumerate(text):
+			results = rg_scorer.score(abstract, sentence)
+			sen_scores[i] = results['rouge1'][2] + 2*results['rouge2'][2] + results['rougeL'][2]
+	sorted_ids = [i[0] for i in sorted(enumerate(sen_scores), key=lambda k: k[1], reverse=True)]
+	sorted_text = [text[i] for i in sorted_ids]
+	return sorted_text
 	
 def topX(text, percentile):
-    if percentile == 1:
-            return text[:math.ceil(len(text)/4)]
-    elif percentile == 2:
-            return text[:math.ceil(len(text)/2)]
-    elif percentile == 3:
-            return text[:math.ceil(len(text)/4*3)]
-    else:
-            raise Exception("invalid percentile")
+	if percentile == 1:
+			return text[:math.ceil(len(text)/4)]
+	elif percentile == 2:
+			return text[:math.ceil(len(text)/2)]
+	elif percentile == 3:
+			return text[:math.ceil(len(text)/4*3)]
+	else:
+			raise Exception("invalid percentile")
 
 def getOne():
-    return 1
+	return 1
 def getTwo():
-    return 2
+	return 2
 def getThree():
-    return 3
+	return 3
 
 def read_args():
-    parser = argparse.ArgumentParser()
-    parser.add_argument("--data_root", type=str, help="")
+	parser = argparse.ArgumentParser()
+	parser.add_argument("--data_root", type=str, help="")
 
-    args, unknown = parser.parse_known_args()
-    return args, unknown
+	args, unknown = parser.parse_known_args()
+	return args, unknown
 
 def main():
 
@@ -52,11 +52,11 @@ def main():
 	args, unknown = read_args()
 
 	train_data = os.path.join(args.data_root, 'train.txt')
-    	val_data = os.path.join(args.data_root, 'val.txt')
-    	test_data = os.path.join(args.data_root, 'test.txt')
+		val_data = os.path.join(args.data_root, 'val.txt')
+		test_data = os.path.join(args.data_root, 'test.txt')
 
 	data_prefixes = ['train', 'val', 'test']
-    	data_paths = [train_data, val_data, test_data]
+		data_paths = [train_data, val_data, test_data]
 	
 	data_path = os.path.join(args.data_root, 'train.txt')
 	task_output_dir = os.path.join(args.data_root, "processed/betterFineTuning")
@@ -86,9 +86,9 @@ def main():
 			.withColumn("top25", F.concat_ws(" ", F.col("top25"))).withColumn("top50", F.concat_ws(" ", F.col("top50"))).withColumn("top75", F.concat_ws(" ", F.col("top75")))
 
 		df.write.json(
-            		path=os.path.join(task_output_dir, prefix),
-            		mode="overwrite")
+					path=os.path.join(task_output_dir, prefix),
+					mode="overwrite")
 
 if __name__ == "__main__":
-    main()
+	main()
 
